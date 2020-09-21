322653b400175fd22c8d573d7145e66b8658ae37
ItssSaul <70920638+ItssSaul@users.noreply.github.com>
2020-09-21 01:22:38 -0500

    Add files via upload

---
+++ LoopsAlgo/algo.js
@@ -0,0 +1,13 @@
+for( var i = 1; i<=20;i++){
+    if( i % 2 === 1){
+        console.log(i);
+    }
+}
+//1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+var num = 0;
+for(var i = 1;i<=5;i++){
+    num+=i;
+    console.log(i);
+    console.log(num);
+}
+//i: 1, Sum: 1, i: 2, Sum: 3, i: 3, Sum: 6, i: 4, Sum: 10, i: 5, Sum: 15
